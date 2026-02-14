efbb264c32f64d9494731e5d6c1e3205a9ba1686
TimovVeen <timovanveen@hotmail.nl>
2026-02-14 01:56:44 +0100

    refactor winit boilerplate

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,29 @@ use std::{num::NonZeroU32, rc::Rc};
 
 use softbuffer::{Context, Surface};
 use vello_cpu::{
-    PaintType, RenderContext, RenderMode,
+    PaintType, RenderContext, RenderMode, RenderSettings,
     color::palette::css,
     kurbo::{Affine, BezPath, Circle, Line, Point, RoundedRect, Shape, Stroke, Vec2},
 };
 use winit::{
+    application::ApplicationHandler,
     event::{KeyEvent, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
+    event_loop::{ActiveEventLoop, EventLoop},
     keyboard::{Key, NamedKey},
-    window::Window,
+    window::{Window, WindowId},
 };
-
-mod app;
 
 const TOLERANCE: f64 = 0.1;
 const STROKE_WIDTH: f64 = 7.;
 const STRIPE_WIDTH: f64 = 3.;
+
+const BEZEL: f64 = 30.;
+const RADIUS: f64 = 60.;
+
+const MARGIN: f64 = 50.;
+const PADDING: f64 = 60.;
+const OFFSET: f64 = 2. * RADIUS + PADDING;
+const MARGIN_OFFSET: f64 = MARGIN + RADIUS;
 
 enum SetShape {
     Diamond,
@@ -29,6 +36,228 @@ enum SetFilling {
     Solid,
     Striped,
     Open,
+}
+
+enum RenderState {
+    Active {
+        window: Rc<Window>,
+        surface: Surface<Rc<Window>, Rc<Window>>,
+    },
+    Suspended,
+}
+
+struct SetApp {
+    render_state: RenderState,
+    renderer: RenderContext,
+    circle: BezPath,
+    rect: BezPath,
+    scale: f64,
+}
+
+impl SetApp {
+    fn new() -> Self {
+        Self {
+            render_state: RenderState::Suspended,
+            renderer: RenderContext::new_with(
+                1980,
+                1080,
+                RenderSettings {
+                    num_threads: 0,
+                    ..Default::default()
+                },
+            ),
+            circle: Circle::new(Point::ZERO, RADIUS).to_path(TOLERANCE),
+            rect: RoundedRect::new(0., 0., 400., 600., BEZEL).to_path(TOLERANCE),
+            scale: 1.,
+        }
+    }
+}
+
+impl ApplicationHandler for SetApp {
+    fn suspended(&mut self, _event_loop: &ActiveEventLoop) {
+        self.render_state = RenderState::Suspended;
+    }
+
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        if matches!(self.render_state, RenderState::Active { .. }) {
+            return;
+        }
+
+        let window = Rc::new(
+            event_loop
+                .create_window(Window::default_attributes())
+                .unwrap(),
+        );
+        let context = Context::new(window.clone()).unwrap();
+        let surface = Surface::new(&context, window.clone()).unwrap();
+
+        self.render_state = RenderState::Active { window, surface };
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &ActiveEventLoop,
+        window_id: WindowId,
+        event: WindowEvent,
+    ) {
+        let RenderState::Active { window, surface } = &mut self.render_state else {
+            return;
+        };
+        if window_id != window.id() {
+            return;
+        }
+        match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        logical_key: Key::Named(NamedKey::Escape),
+                        ..
+                    },
+                ..
+            } => {
+                event_loop.exit();
+            }
+            WindowEvent::ScaleFactorChanged {
+                scale_factor,
+                inner_size_writer: _,
+            } => {
+                self.scale = scale_factor;
+                println!("{scale_factor}");
+            }
+            WindowEvent::Resized(size) => {
+                let width = size.width.max(1);
+                let height = size.height.max(1);
+
+                surface
+                    .resize(
+                        NonZeroU32::new(width).unwrap(),
+                        NonZeroU32::new(height).unwrap(),
+                    )
+                    .unwrap();
+
+                self.renderer = RenderContext::new_with(
+                    width as u16,
+                    height as u16,
+                    RenderSettings {
+                        num_threads: 0,
+                        ..Default::default()
+                    },
+                );
+
+                window.request_redraw();
+            }
+            WindowEvent::RedrawRequested => {
+                self.renderer.reset();
+
+                let size = window.inner_size();
+
+                let mut buffer = surface.buffer_mut().unwrap();
+
+                let bufslice = {
+                    let len = buffer.len() * 4;
+                    let ptr = buffer.as_mut_ptr() as *mut u8;
+                    unsafe { std::slice::from_raw_parts_mut(ptr, len) }
+                };
+
+                self.renderer.set_paint(css::WHITE);
+                self.renderer.fill_path(&self.rect);
+
+                self.renderer
+                    .set_transform(Affine::translate(Vec2::new(MARGIN_OFFSET, MARGIN_OFFSET)));
+                self.renderer.set_paint(css::RED);
+                self.renderer.fill_path(&self.circle);
+
+                self.renderer.set_transform(Affine::translate(Vec2::new(
+                    MARGIN_OFFSET,
+                    MARGIN_OFFSET + OFFSET,
+                )));
+                self.renderer.set_paint(css::YELLOW);
+                self.renderer.fill_path(&self.circle);
+
+                self.renderer.set_transform(Affine::translate(Vec2::new(
+                    MARGIN_OFFSET,
+                    MARGIN_OFFSET + OFFSET * 2.,
+                )));
+                self.renderer.set_paint(css::BLUE);
+                self.renderer.fill_path(&self.circle);
+
+                self.renderer.set_transform(Affine::translate(Vec2::new(
+                    MARGIN_OFFSET + OFFSET,
+                    MARGIN_OFFSET,
+                )));
+                self.renderer.set_paint(css::ORANGE);
+                self.renderer.fill_path(&self.circle);
+
+                self.renderer.set_transform(Affine::translate(Vec2::new(
+                    MARGIN_OFFSET + OFFSET,
+                    MARGIN_OFFSET + OFFSET,
+                )));
+                self.renderer.set_paint(css::GREEN);
+                self.renderer.fill_path(&self.circle);
+
+                self.renderer.set_transform(Affine::translate(Vec2::new(
+                    MARGIN_OFFSET + OFFSET,
+                    MARGIN_OFFSET + OFFSET * 2.,
+                )));
+                self.renderer.set_paint(css::PURPLE);
+                self.renderer.fill_path(&self.circle);
+
+                self.renderer
+                    .set_transform(Affine::scale(0.8).with_translation(Vec2::new(440., 460.)));
+                self.renderer.set_paint(css::WHITE);
+                self.renderer.fill_path(&self.rect);
+
+                self.renderer
+                    .set_transform(Affine::translate(Vec2::new(500., 500.)));
+                draw_shape(
+                    &mut self.renderer,
+                    SetShape::Oval,
+                    SetFilling::Striped,
+                    css::RED,
+                );
+
+                self.renderer
+                    .set_transform(Affine::translate(Vec2::new(500., 650.)));
+                draw_shape(
+                    &mut self.renderer,
+                    SetShape::Diamond,
+                    SetFilling::Open,
+                    css::GREEN,
+                );
+
+                self.renderer
+                    .set_transform(Affine::translate(Vec2::new(500., 800.)));
+                draw_shape(
+                    &mut self.renderer,
+                    SetShape::Squiggle,
+                    SetFilling::Solid,
+                    css::REBECCA_PURPLE,
+                );
+
+                self.renderer.flush();
+                self.renderer.render_to_buffer(
+                    bufslice,
+                    size.width as u16,
+                    size.height as u16,
+                    RenderMode::OptimizeSpeed,
+                );
+
+                // Convert BGRA to RGBA
+                unsafe {
+                    bufslice
+                        .as_chunks_unchecked_mut::<4>()
+                        .iter_mut()
+                        .for_each(|chunk| {
+                            chunk.swap(0, 2);
+                        });
+                }
+
+                buffer.present().unwrap();
+            }
+            _ => {}
+        }
+    }
 }
 
 fn draw_shape(
@@ -85,164 +314,6 @@ fn draw_shape(
 
 fn main() {
     let event_loop = EventLoop::new().unwrap();
-    let context = Context::new(event_loop.owned_display_handle()).unwrap();
-
-    let bezel = 30.;
-    let radius = 60.;
-
-    let margin = 50.;
-    let padding = 60.;
-    let offset = 2. * radius + padding;
-    let margin_offset = margin + radius;
-
-    let rect = RoundedRect::new(0., 0., 400., 600., bezel).to_path(TOLERANCE);
-    let circle = Circle::new(Point::ZERO, radius).to_path(TOLERANCE);
-
-    let mut app = app::WinitAppBuilder::with_init(
-        |elwt| {
-            let window = elwt.create_window(Window::default_attributes());
-            Rc::new(window.unwrap())
-        },
-        |_elwt, window| Surface::new(&context, window.clone()).unwrap(),
-    )
-    .with_event_handler(|window, surface, window_id, event, elwt| {
-        elwt.set_control_flow(ControlFlow::Wait);
-
-        if window_id != window.id() {
-            return;
-        }
-
-        match event {
-            WindowEvent::RedrawRequested => {
-                let Some(surface) = surface else {
-                    eprintln!("RedrawRequested fired before Resumed or after Suspended");
-                    return;
-                };
-                // TODO: resizes surface every frame, use Resized window event instead
-                let size = window.inner_size();
-                surface
-                    .resize(
-                        NonZeroU32::new(size.width).unwrap(),
-                        NonZeroU32::new(size.height).unwrap(),
-                    )
-                    .unwrap();
-
-                let mut buffer = surface.buffer_mut().unwrap();
-
-                let bufslice = {
-                    let len = buffer.len() * 4;
-                    let ptr = buffer.as_mut_ptr() as *mut u8;
-                    unsafe { std::slice::from_raw_parts_mut(ptr, len) }
-                };
-
-                {
-                    let mut renderctx = RenderContext::new(size.width as u16, size.height as u16);
-
-                    renderctx.set_paint(css::WHITE);
-                    renderctx.fill_path(&rect);
-
-                    renderctx
-                        .set_transform(Affine::translate(Vec2::new(margin_offset, margin_offset)));
-                    renderctx.set_paint(css::RED);
-                    renderctx.fill_path(&circle);
-
-                    renderctx.set_transform(Affine::translate(Vec2::new(
-                        margin_offset,
-                        margin_offset + offset,
-                    )));
-                    renderctx.set_paint(css::YELLOW);
-                    renderctx.fill_path(&circle);
-
-                    renderctx.set_transform(Affine::translate(Vec2::new(
-                        margin_offset,
-                        margin_offset + offset * 2.,
-                    )));
-                    renderctx.set_paint(css::BLUE);
-                    renderctx.fill_path(&circle);
-
-                    renderctx.set_transform(Affine::translate(Vec2::new(
-                        margin_offset + offset,
-                        margin_offset,
-                    )));
-                    renderctx.set_paint(css::ORANGE);
-                    renderctx.fill_path(&circle);
-
-                    renderctx.set_transform(Affine::translate(Vec2::new(
-                        margin_offset + offset,
-                        margin_offset + offset,
-                    )));
-                    renderctx.set_paint(css::GREEN);
-                    renderctx.fill_path(&circle);
-
-                    renderctx.set_transform(Affine::translate(Vec2::new(
-                        margin_offset + offset,
-                        margin_offset + offset * 2.,
-                    )));
-                    renderctx.set_paint(css::PURPLE);
-                    renderctx.fill_path(&circle);
-
-                    renderctx
-                        .set_transform(Affine::scale(0.8).with_translation(Vec2::new(440., 460.)));
-                    renderctx.set_paint(css::WHITE);
-                    renderctx.fill_path(&rect);
-
-                    renderctx.set_transform(Affine::translate(Vec2::new(500., 500.)));
-                    draw_shape(
-                        &mut renderctx,
-                        SetShape::Oval,
-                        SetFilling::Striped,
-                        css::RED,
-                    );
-
-                    renderctx.set_transform(Affine::translate(Vec2::new(500., 650.)));
-                    draw_shape(
-                        &mut renderctx,
-                        SetShape::Diamond,
-                        SetFilling::Open,
-                        css::GREEN,
-                    );
-
-                    renderctx.set_transform(Affine::translate(Vec2::new(500., 800.)));
-                    draw_shape(
-                        &mut renderctx,
-                        SetShape::Squiggle,
-                        SetFilling::Solid,
-                        css::REBECCA_PURPLE,
-                    );
-
-                    renderctx.render_to_buffer(
-                        bufslice,
-                        size.width as u16,
-                        size.height as u16,
-                        RenderMode::OptimizeSpeed,
-                    );
-                }
-                // Convert BGRA to RGBA
-                unsafe {
-                    bufslice
-                        .as_chunks_unchecked_mut::<4>()
-                        .iter_mut()
-                        .for_each(|chunk| {
-                            chunk.swap(0, 2);
-                        });
-                }
-
-                buffer.present().unwrap();
-            }
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        logical_key: Key::Named(NamedKey::Escape),
-                        ..
-                    },
-                ..
-            } => {
-                elwt.exit();
-            }
-            _ => {}
-        }
-    });
-
+    let mut app = SetApp::new();
     event_loop.run_app(&mut app).unwrap();
 }
